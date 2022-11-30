8d70a90c2d16676a2461c9287d4d2d08a81c3825
Alex <naut@outlook.com>
2022-11-30 01:57:01 -0600

    add value to board when specific part of grid is clicked

--- tictactoe/tictac.py
+++ tictactoe/tictac.py
@@ -27,6 +27,7 @@ def draw_grid():
             rect = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
             pygame.draw.rect(SCREEN, WHITE, rect, 1)
 
+
 # Board stuff
 board = []
 
@@ -35,12 +36,59 @@ for i in range(9):
 
 print(board)
 
+# Game variables
+active_player = 0 
+
 running = True
+
+def place_piece(pos):
+    x = 0 # x-axis
+    y = 1 # y-axis
+    
+    # I should find a cleaner way to do this
+    if pos[y] in range(100, 0, -1):
+        if pos[x] in range(100, 0, -1):
+            board[0] = active_player
+
+        if pos[x] in range(200, 100, -1):
+            board[1] = active_player
+
+        if pos[x] in range(300, 200, -1):
+            board[2] = active_player
+
+    if pos[y] in range(200, 100, -1):
+        if pos[x] in range(100, 0, -1):
+            board[3] = active_player
+
+        if pos[x] in range(200, 100, -1):
+            board[4] = active_player
+
+        if pos[x] in range(300, 200, -1):
+            board[5] = active_player
+
+    if pos[y] in range(300, 200, -1):
+        if pos[x] in range(100, 0, -1):
+            board[6] = active_player
+
+        if pos[x] in range(200, 100, -1):
+            board[7] = active_player
+
+        if pos[x] in range(300, 200, -1):
+            board[8] = active_player
+    print(board) 
+
+def check_winner():
+    pass
 
 # Main loop
 while running: 
     # Event loop
     for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            print(pos)
+            place_piece(pos)            
+
         if event.type == pygame.KEYDOWN:
             if event.key in [K_ESCAPE, K_q]:
                 running = False
