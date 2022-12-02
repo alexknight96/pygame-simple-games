9156b8eba0f1c94cb65cce050a1825a1d9e07033
Alex <naut@outlook.com>
2022-12-02 03:17:21 -0600

    code optimization

--- tictactoe/tictac.py
+++ tictactoe/tictac.py
@@ -32,49 +32,33 @@ def draw_grid():
             rect = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
             pygame.draw.rect(SCREEN, WHITE, rect, 1)
 
+def mouse_pos():
+    # Takes the mouse position and, if over a tile, returns the tile index relative to the boardlist
+    pos = pygame.mouse.get_pos()
+    print(pos)
+    x = pos[0] // BLOCK_SIZE
+    y = pos[1] // BLOCK_SIZE
 
-def place_piece(pos):
+    return x, y
+
+def modify_board():
     global active_player
 
-    def place_x(position, a, b, c):
-        if position in range(100, 0, -1):
-            board[a] = active_player
+    x, y = mouse_pos()
+    board[y][x] = active_player
 
-        if position in range(200, 100, -1):
-            board[b] = active_player
-
-        if position in range(300, 200, -1):
-            board[c] = active_player
-    
-    # For readability 
-    x = 0 # x-axis
-    y = 1 # y-axis
-
-    # Finds out which square is clicked and places the active_player in the appropriate list element. 
-    if pos[y] in range(100, 0, -1):
-        place_x(pos[x], 0, 1, 2) 
-
-    if pos[y] in range(200, 100, -1):
-        place_x(pos[x], 3, 4, 5)
-
-    if pos[y] in range(300, 200, -1):
-        place_x(pos[x], 6, 7, 8)
-    
-    print(board)
-
-    # Cycles between the X/O pieces each time a piece is placed
     if active_player == 0:
         active_player = 1
 
     else:
         active_player = 0
 
-# Board set-up 
-board = []
+    print(board)
 
-# Fill the board list with 9 (for each square on the grid) empty elements
-for i in range(9):
-    board.append('')
+# Board set-up 
+board = [['', '', ''],
+         ['', '', ''],
+         ['', '', '']]
 
 running = True
 
@@ -87,9 +71,7 @@ while running:
     # Event loop
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONUP:
-            pos = pygame.mouse.get_pos()
-            print(pos)
-            place_piece(pos)
+            modify_board() 
 
         if event.type == pygame.KEYDOWN:
             if event.key in [K_ESCAPE, K_q]:
